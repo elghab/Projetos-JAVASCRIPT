a47a369c3f9a5cda0d97652f9459a0f2e47c7933
elghab <gabrielc.contato@gmail.com>
2023-06-09 11:22:38 -0300

    adição de anotaçoes e projeto pokedex

---
+++ Logica-ALURA/Anotações/Funcoes-javaScript.js
@@ -0,0 +1,29 @@
+//Funções de Array
+array.forEach()
+array.map()
+array.filter()
+array.sort()
+array.reduce()
+
+//Funções de Math
+Math.round()
+Math.random()
+Math.floor()
+Math.sqrt()
+Math.sin()
+
+//Funçoes de Date
+Date.now()
+Date.getSeconds()
+Date.getMinutes()
+Date.getHours()
+Date.getFullYear()
+
+//Funções de String
+string.indexOf()
+string.toUpperCase()
+string.toLowerCase()
+string.replace()
+string.replaceAll()
+
+
